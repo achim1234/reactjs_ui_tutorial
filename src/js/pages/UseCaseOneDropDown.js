2108675d22f43730e32f6bf9c470338c8a45b76e
achim <astrohm@gmx.net>
2017-08-22 12:10:06 +0200

    UseCase hinzugefügt 3

--- src/js/pages/UseCaseOneDropDown.js
+++ src/js/pages/UseCaseOneDropDown.js
@@ -5,13 +5,61 @@ import Button from "../components/tutorial/revolverButton_useCase/RevolverButton
 export default class UseCaseOneDropDown extends React.Component {
   render() {
 
+    const style1 = {
+      backgroundColor: '#EF9A9A',
+      color: 'white'
+    };
+    const style2 = {
+      backgroundColor: '#E91E63',
+      color: 'white'
+    };
+    const style3 = {
+      backgroundColor: '#4527A0',
+      color: 'white'
+    };
+    const style4 = {
+      backgroundColor: '#006064',
+      color: 'white'
+    };
+    const style5 = {
+      backgroundColor: '#03A9F4',
+      color: 'white'
+    };
+    const style6 = {
+      backgroundColor: '#00C853',
+      color: 'white'
+    };
+
+    const shopStyle = {
+      backgroundColor: 'LightCyan'
+    };
+
+
+
     return (
       <div>
         <h2>Use Case 1 (Drop Down)</h2>
         <p>In diesem Use Case soll in einem fiktiven Shop die Farbe eines Kugelschreibers ausgewählt werden.</p>
+        <br />
+        <div style={shopStyle}>
+          <h1>Kugelschreiber Shop</h1>
+          <p>Beschreibung: Kugelschreiber mit vielen verschiedenen Farben!</p>
 
-        
-    	
+
+          
+          <p>Farbauswahl:</p>
+          <select>
+            <option style={style1} value="hellpink">helles Pink</option>
+            <option style={style2} value="pink">Pink</option>
+            <option style={style3} value="lila">Lila</option>
+            <option style={style4} value="dunkelgruen">dunkel Grün</option>
+            <option style={style5} value="blau">Blau</option>
+            <option style={style6} value="gruen">Grün</option>
+          </select> 
+          <br /><br /><br /><br />
+          <button type="button">Bestellen</button>
+          <br />
+    	 </div>
       </div>
     );
   }
--- src/js/pages/UseCaseOneRevolverButton.js
+++ src/js/pages/UseCaseOneRevolverButton.js
@@ -4,13 +4,24 @@ import Button from "../components/tutorial/revolverButton_useCase/RevolverButton
 export default class UseCaseOneRevolverButton extends React.Component {
   render() {
 
+    const shopStyle = {
+      backgroundColor: 'LightCyan'
+    };
+
     return (
       <div>
         <h2>Use Case 1 (Revolver Button)</h2>
         <p>In diesem Use Case soll in einem fiktiven Shop die Farbe eines Kugelschreibers ausgewählt werden.</p>
-        
-        <Button htmlElement="" btnName="Farbe" maxItems="6" values={['#EF9A9A', '#E91E63', '#4527A0', '#006064', '#03A9F4', '#00C853']} />
-    	
+
+        <div style={shopStyle}>
+          <h1>Kugelschreiber Shop</h1>
+          <p>Beschreibung: Kugelschreiber mit vielen verschiedenen Farben!</p>
+
+          <Button htmlElement="" btnName="Farbe" maxItems="6" values={['#EF9A9A', '#E91E63', '#4527A0', '#006064', '#03A9F4', '#00C853']} />
+          <br /><br /><br /><br />
+          <button type="button">Bestellen</button>
+          <br />
+       </div>
       </div>
     );
   }
